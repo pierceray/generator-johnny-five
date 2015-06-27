e395bdc1dd2acbd83a8ebbdf62a26e14f6041c45
Ray Pierce <ray@digitalpierce.com>
2015-06-27 00:08:49 -0500

    initial commit for the generator

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# generator-johnny-five
-Johnny Five Yeoman Generator
+# generator-johnny-five 
+
+> [Yeoman](http://yeoman.io) generator that scaffolds out a [Johnny-Five](http://johnny-five.io) project
+
+## Features
+* Johnny-Five project file creation
+* `.jshintrc` from the Johnny-Five project
+* `package.json` creation and dependency additions
+* Optional Library Installs
+	* barcli
+	* node-pixel
+
+
+## Getting Started
+
+Not every new computer comes with a Yeoman pre-installed.
+
+```
+npm install -g yo
+```
+
+### Yeoman Generators
+
+Yeoman travels light. He didn't pack any generators when he moved in. You can think of a generator like a plug-in. You get to choose what type of application you wish to create, such as a Backbone application or even a Chrome extension.
+
+To install generator-johnny-five from npm, run:
+
+```
+npm install -g generator-johnny-five
+```
+
+Finally, initiate the generator:
+
+```
+yo johnny-five
+```
+
+## License
+
+MIT
---
+++ generators/app/index.js
@@ -0,0 +1,88 @@
+'use strict';
+var generators = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = generators.Base.extend({
+
+	constructor: function(){
+		generators.Base.apply(this, arguments);
+	},
+
+	prompting: function () {
+		var done = this.async();
+
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'Welcome to the ' + chalk.red('Johnny-Five') + ' generator!'
+		));
+
+		var prompts = [{
+			type: 'checkbox',
+			name: 'features',
+			message: 'What additional features would you like installed?',
+			choices: [
+				{
+					name: 'nodepixel',
+					value: 'includeNodePixel',
+					checked: false
+				}
+				,{
+					name: 'barcli',
+					value: 'includeBarcli',
+					checked: false
+				}
+			]
+		}];
+
+		this.prompt(prompts, function (answers) {
+			var features = answers.features;
+
+			function hasFeature(feat) {
+				return features && features.indexOf(feat) !== -1;
+			};
+
+			this.includeNodePixel = hasFeature('includeNodePixel');
+			this.includeBarcli = hasFeature('includeBarcli');
+
+			done();
+		}.bind(this));
+	},
+
+	writing: {
+		app: function () {
+			this.fs.copyTpl(
+				this.templatePath('_package.json'),
+				this.destinationPath('package.json'),
+				{
+					appname: this.appname
+				}
+			);
+			this.fs.copy(
+				this.templatePath('_index.js'),
+				this.destinationPath('index.js')
+			);
+		},
+
+		projectfiles: function () {
+			this.fs.copy(
+				this.templatePath('jshintrc'),
+				this.destinationPath('.jshintrc')
+			);
+		}
+	},
+
+	install: function () {
+
+		var moduleArray = ['johnny-five'];
+		if(this.includeNodePixel){
+			moduleArray.push('node-pixel');
+		}
+
+		if(this.includeBarcli){
+			moduleArray.push('barcli');
+		}
+
+		this.npmInstall(moduleArray.join(' '), { 'save': true });
+	}
+});
