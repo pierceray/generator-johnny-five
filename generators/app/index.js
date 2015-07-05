2323681193be7a3b1e7fafe2bc477398a2ee686a
Ray Pierce <ray@digitalpierce.com>
2015-07-05 18:46:52 -0500

    changed to a list to determine project type

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,11 +6,11 @@ var _s = require('underscore.string');
 
 module.exports = generators.Base.extend({
 
-	constructor: function(){
+	constructor: function() {
 		generators.Base.apply(this, arguments);
 	},
 
-	prompting: function () {
+	prompting: function() {
 		var done = this.async();
 
 		// Have Yeoman greet the user.
@@ -19,50 +19,48 @@ module.exports = generators.Base.extend({
 		));
 
 		var prompts = [{
+			type: 'list',
+			name: 'projectType',
+			message: 'What type of project would you like to create?',
+			choices: [{
+				name: 'Johnny-Five (standard)',
+				value: 'useJohnnyFive'
+			}, {
+				name: 'Particle (formerly Spark) microcontroller',
+				value: 'useParticle'
+			}, {
+				name: 'Raspberry Pi',
+				value: 'useRasPi'
+			}]
+		}, {
+			type: 'checkbox',
+			name: 'features',
+			message: 'What additional libraries would you like installed with your project?',
+			choices: [{
+				name: 'barcli',
+				value: 'includeBarcli',
+				checked: false
+			}, {
+				name: 'j5-songs',
+				value: 'includej5Songs',
+				checked: false
+			}, {
+				name: 'nodepixel',
+				value: 'includeNodePixel',
+				checked: false
+			}, {
+				name: 'oled-js',
+				value: 'includeOledJS',
+				checked: false
+			}]
+		}, {
 			type: 'string',
 			name: 'licenseType',
 			message: 'What type of license should this project have?',
 			default: 'MIT'
-		},{
-			type: 'confirm',
-			name: 'useParticle',
-			message: 'Are you using a Particle (formerly Spark) microcontroller?',
-			default: false
-		},{
-			type: 'confirm',
-			name: 'useRasPi',
-			message: 'Are you using a Raspberry Pi?',
-			default: false,
-			when: function( response ){ return !response.useParticle; }
-		},{
-			type: 'checkbox',
-			name: 'features',
-			message: 'What additional libraries would you like installed?',
-			choices: [
-				{
-					name: 'barcli',
-					value: 'includeBarcli',
-					checked: false
-				},
-				{
-					name: 'j5-songs',
-					value: 'includej5Songs',
-					checked: false
-				},
-				{
-					name: 'nodepixel',
-					value: 'includeNodePixel',
-					checked: false
-				},
-				{
-					name: 'oled-js',
-					value: 'includeOledJS',
-					checked: false
-				}
-			]
 		}];
 
-		this.prompt(prompts, function ( answers ) {
+		this.prompt(prompts, function(answers) {
 			var features = answers.features;
 
 			function hasFeature(feat) {
@@ -70,8 +68,7 @@ module.exports = generators.Base.extend({
 			};
 
 			this.licenseType = answers.licenseType;
-			this.useParticle = answers.useParticle;
-			this.useRasPi = answers.useRasPi;
+			this.projectType = answers.projectType;
 			this.includeBarcli = hasFeature('includeBarcli');
 			this.includej5Songs = hasFeature('includej5Songs');
 			this.includeNodePixel = hasFeature('includeNodePixel');
@@ -84,36 +81,38 @@ module.exports = generators.Base.extend({
 	},
 
 	writing: {
-		app: function () {
-			this.template( '_package.json', 'package.json' );
-			this.template( '_index.js', 'index.js' );
+		app: function() {
+			this.template('_package.json', 'package.json');
+			this.template('_index.js', 'index.js');
 		},
 
-		projectfiles: function () {
-			this.copy('jshintrc','.jshintrc');
+		projectfiles: function() {
+			this.copy('jshintrc', '.jshintrc');
 		}
 	},
 
-	install: function () {
+	install: function() {
 
 		var moduleArray = ['johnny-five'];
 
-		if(this.includeNodePixel){
+		if (this.includeNodePixel) {
 			moduleArray.push('node-pixel');
 		}
 
-		if(this.includej5Songs){
+		if (this.includej5Songs) {
 			moduleArray.push('j5-songs');
 		}
 
-		if(this.includeBarcli){
+		if (this.includeBarcli) {
 			moduleArray.push('barcli');
 		}
 
-		if(this.includeOledJS){
+		if (this.includeOledJS) {
 			moduleArray.push('oled-js');
 		}
 
-		this.npmInstall(moduleArray.join(' '), { 'save': true });
+		this.npmInstall(moduleArray.join(' '), {
+			'save': true
+		});
 	}
 });
--- generators/app/templates/_index.js
+++ generators/app/templates/_index.js
@@ -1,45 +1,77 @@
-var five = require("johnny-five");
-<% if ( useParticle ) { %>var board = new five.Board({
+var five = require("johnny-five"); <%
+if (projectType === 'useParticle') { %>
+var board = new five.Board({
   io: new Spark({
     token: process.env.SPARK_TOKEN,
     deviceId: process.env.SPARK_DEVICE_ID
   });
-});<% } else if ( useRasPi ) { %>var board = new five.Board({
-  io: new Raspi()
-});<% } else { %>var board = new five.Board();<% } %>
+}); <% }
 
-<% if ( includeBarcli ) { %>var Barcli = require("barcli");<% } %>
-<% if ( includej5Songs ){ %>var songs = require("j5-songs");<% } %>
-<% if ( includeNodePixel ){ %>var pixel = require("node-pixel");<% } %>
-<% if ( includeOledJS ) { %>var Oled = require("oled-js");<% } %>
-<% if ( includeNodePixel ){ %>var strip = null;<% } %>
+if (projectType === 'useRasPi') { %>
+  var board = new five.Board({
+    io: new Raspi()
+  });
+<% } else { %>
+var board = new five.Board();
+<% }
+
+if (includeBarcli) { %>
+var Barcli = require("barcli");
+<% }
+
+if (includej5Songs) { %>
+var songs = require("j5-songs");
+<% }
+
+if (includeNodePixel) { %>
+var pixel = require("node-pixel");
+var strip = null;
+<% }
+
+if (includeOledJS) { %>
+var Oled = require("oled-js");
+<% } %>
 
 // The board's pins will not be accessible until
 // the board has reported that it is ready
-board.on("ready", function() {
+board.on("ready", function () {
   console.log("Ready!");
-  <% if ( includeBarcli ) { %>var graph = new Barcli();
+<% if (includeBarcli) { %>
+  var graph = new Barcli();
+  // Sets bar to 25%
+  graph.update(0.25);
+  // Sets bar to 100%
+  graph.update(1.0);
+<% }
 
-  graph.update(0.25); // Sets bar to 25%
-  graph.update(1.0); // Sets bar to 100%<% } %>
-  <% if ( includeOledJS ) { %>var opts = {
+if (includeOledJS) { %>
+  //OLED Options
+  var opts = {
     width: 128,
     height: 64,
     address: 0x3D
   };
 
-  var oled = new Oled(board, five, opts);<% } %>
-  <% if ( includeNodePixel ){ %>strip = new pixel.Strip({
-        data: 6,
-        length: 4,
-        firmata: board,
-        controller: "FIRMATA",
-    });
+  var oled = new Oled(board, five, opts);
+<% }
 
-    strip.on("ready", function() {
-        // do stuff with the strip here.
-    });<% } %>
-  <% if ( includej5Songs ) { %>var piezo = new five.Piezo(3);
+if (includeNodePixel) { %>
+  // Node-Pixel options
+  strip = new pixel.Strip({
+    data: 6,
+    length: 4,
+    firmata: board,
+    controller: "FIRMATA",
+  });
+
+  strip.on("ready", function () {
+    // do stuff with the strip here.
+  });
+<% }
+
+if (includej5Songs) { %>
+  // j5-songs
+  var piezo = new five.Piezo(3);
   // Load a song object
   var song = songs.load('never-gonna-give-you-up');
 
@@ -48,8 +80,13 @@ board.on("ready", function() {
 
   // List all songs
   songs.list(function (err, tunes) {
-  // Object literal with all the songs
-  });<% } %>
-  <% if ( !(includej5Songs || includeOledJS || includeNodePixel)) { %>var led = new five.Led(13);
-  led.blink(500);<% } %>
+  s// Object literal with all the songs
+  });
+<% }
+
+if (!(includej5Songs || includeOledJS || includeNodePixel)) { %>
+
+  var led = new five.Led(13);
+  led.blink(500); <%
+} %>
 });
