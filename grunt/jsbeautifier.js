97258cee27ab5eecac0eb39ebbe51eb552b6a90f
Ray Pierce <ray@digitalpierce.com>
2015-07-11 23:25:05 -0500

    added aliases.yaml and made two targets for jsbeautifer

--- grunt/jsbeautifier.js
+++ grunt/jsbeautifier.js
@@ -1,31 +1,45 @@
+var jsbFiles = [
+  'Gruntfile.js',
+  'grunt/**/*.js',
+  'generators/**/*.js',
+  //blacklist
+  '!**/node_modules/**',
+  '!**/templates/**'
+];
+
+var jsbOptions = {
+  braceStyle: 'collapse',
+  breakChainedMethods: false,
+  e4x: false,
+  evalCode: false,
+  indentChar: ' ',
+  indentLevel: 0,
+  indentSize: 2,
+  indentWithTabs: false,
+  jslintHappy: false,
+  keepArrayIndentation: false,
+  keepFunctionIndentation: false,
+  maxPreserveNewlines: 10,
+  preserveNewlines: true,
+  spaceBeforeConditional: true,
+  spaceInParen: false,
+  unescapeStrings: false,
+  wrapLineLength: 0
+};
+
 module.exports = {
-  files: [
-    'Gruntfile.js',
-    'grunt/**/*.js',
-    'generators/**/*.js',
-    //blacklist
-    '!**/node_modules/**',
-    '!**/templates/**'
-  ],
-  options: {
-    js: {
-      braceStyle: "collapse",
-      breakChainedMethods: false,
-      e4x: false,
-      evalCode: false,
-      indentChar: " ",
-      indentLevel: 0,
-      indentSize: 2,
-      indentWithTabs: false,
-      jslintHappy: false,
-      keepArrayIndentation: false,
-      keepFunctionIndentation: false,
-      maxPreserveNewlines: 10,
-      preserveNewlines: true,
-      spaceBeforeConditional: true,
-      spaceInParen: false,
-      unescapeStrings: false,
-      wrapLineLength: 0
+  verify: {
+    src: jsbFiles,
+    options: {
+      js: jsbOptions,
+      mode: 'VERIFY_ONLY'
+    }
+  },
+  write: {
+    src: jsbFiles,
+    options: {
+      js: jsbOptions,
+      mode: 'VERIFY_AND_WRITE'
     }
   }
-}
+};
